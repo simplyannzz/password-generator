4c143c36655297a74536fd01dbc4c407535ae172
Annie <anniebui0110@gmil.com>
2023-07-04 09:53:23 -0500

    the password generator works

--- Develop/script.js
+++ Develop/script.js
@@ -10,24 +10,24 @@ function generatePassword() {
     symbols: "!@#$%^&*()_+~`|}{[]:;?><,./-=",
   };
   // to randomly select types
-  const getType = [
-    function uppercase() {
+  //const getType = [
+    function uppercaseGen() {
       return types.uppercase[
-        Math.floor(math.random() * types.uppercase.length)
+        Math.floor(Math.random() * types.uppercase.length)
       ];
-    },
-    function lowerCase() {
+    }
+    function lowerCaseGen() {
       return types.lowerCase[
-        Math.floor(math.random() * types.lowerCase.length)
+        Math.floor(Math.random() * types.lowerCase.length)
       ];
-    },
-    function number() {
-      return types.numbers[Math.floor(math.random() * types.numbers.length)];
-    },
-    function symbols() {
-      return types.symbols[Math.floor(math.random() * types.symbols.length)];
-    },
-  ];
+    }
+    function numberGen() {
+      return types.numbers[Math.floor(Math.random() * types.numbers.length)];
+    }
+    function symbolsGen() {
+      return types.symbols[Math.floor(Math.random() * types.symbols.length)];
+    }
+ // ];
   // to see if the checkbox is checked
   let number = document.getElementById("includenumbers").checked;
   let symbols = document.getElementById("includesymbols").checked;
@@ -40,16 +40,30 @@ console.log (number)
     return;
   }
   let passwordbox = document.getElementById("password");
-  let length = document.getElementById("length");
+  let length = document.getElementById("charactersAmountRange").value;
   let pw = "";
-  while (pw.length < length.value) {
-    let typeAdder = getType[match.floor(match.random() * getType.length)];
-    let isChecked = document.getElementById(typeAdder.name).checked;
-    if (isChecked) {
-      pw += typeAdder();
+  while (pw.length < length) {
+    // let typeAdder = getType[match.floor(match.random() * getType.length)];
+    // let isChecked = document.getElementById(typeAdder.name).checked;
+    // if (isChecked) {
+    //   pw += typeAdder();
+    // }
+    if(pw.length<length && number){
+      pw += numberGen()
     }
+    if(pw.length<length && uppercase){
+      pw += uppercaseGen()
+    }
+    if(pw.length<length && lowerCase){
+      pw += lowerCaseGen()
+    }
+    if(pw.length<length && symbols){
+      pw += symbolsGen()
+    }
+
   }
-  passwordbox.innerHTML = pw;
+  console.log(pw,length)
+ return pw;
 }
 
 //not sure what to do here lol
